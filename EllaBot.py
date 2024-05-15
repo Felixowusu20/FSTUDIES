16f64d5b2312a29ead3a010039836409baf4f81d
Felix <146973549+Felixowusu20@users.noreply.github.com>
2024-05-15 03:15:52 +0000

    Update EllaBot.py
    second time given the api

--- EllaBot.py
+++ EllaBot.py
@@ -23,7 +23,7 @@ st.sidebar.info(
 )
 
 # Initialize the OpenAI client
-client = OpenAI(api_key= "Enter your api key here")
+client = OpenAI(api_key= "sk-proj-TwRmFL05p6hOHS51SSSyT3BlbkFJdO9MqczzeXKPXchVZaBb")
 
 # Main chat interface
 st.subheader("Hi ,I'm Ella Your Daily Companion.")
